7fb96432729e272919f662fd52d018660be821f8
Hannes Moser <hannes@impossiblearts.com>
2016-02-12 15:29:12 +0100

    test: better assertions for `read` integration tests

--- test/methods/read.js
+++ test/methods/read.js
@@ -82,28 +82,85 @@ describe('netiam', () => {
         .expect(200)
         .expect('Content-Type', /json/)
         .expect(res => {
-          res.body.should.be.an.Object()
-          res.body.should.have.properties(['data'])
-          //console.log(util.inspect(res.body, {depth: null}))
-        })
-        .end(done)
-    })
+          const json = res.body
 
-    it.skip('should get a user by ID', done => {
-      request(app)
-        .get(`/users/${user.id}`)
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(res => {
-          res.body.should.have.properties([
+          json.should.be.an.Object()
+          json.should.have.properties(['data', 'included'])
+          json.data.should.be.Array()
+          json.data.should.have.length(1)
+          json.data[0].should.be.Object()
+          json.data[0].should.have.properties([
             'id',
+            'type',
+            'attributes',
+            'relationships'
+          ])
+          json.data[0].id.should.be.String()
+          json.data[0].id.should.have.length(36)
+          json.data[0].type.should.be.String()
+          json.data[0].type.should.eql('user')
+          json.data[0].attributes.should.be.Object()
+          json.data[0].attributes.should.have.properties([
             'email',
             'username',
             'birthday',
             'createdAt',
             'updatedAt'
           ])
+          json.data[0].attributes.email.should.eql('hannes@impossiblearts.com')
+          json.data[0].attributes.username.should.eql('eliias')
+          json.data[0].relationships.should.be.Object()
+          json.data[0].relationships.should.have.properties([
+            'campaigns',
+            'projects'
+          ])
+          json.included.should.be.Array()
+          json.included.should.have.length(1)
+          json.included[0].type.should.eql('project')
+        })
+        .end(done)
+    })
+
+    it('should get a user by ID', done => {
+      request(app)
+        .get(`/users/${user.id}`)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(res => {
+          const json = res.body
+
+          json.should.have.properties(['data', 'included'])
+          json.data.should.be.Object()
+          json.data.should.be.Object()
+          json.data.should.have.properties([
+            'id',
+            'type',
+            'attributes',
+            'relationships'
+          ])
+          json.data.id.should.be.String()
+          json.data.id.should.have.length(36)
+          json.data.type.should.be.String()
+          json.data.type.should.eql('user')
+          json.data.attributes.should.be.Object()
+          json.data.attributes.should.have.properties([
+            'email',
+            'username',
+            'birthday',
+            'createdAt',
+            'updatedAt'
+          ])
+          json.data.attributes.email.should.eql('hannes@impossiblearts.com')
+          json.data.attributes.username.should.eql('eliias')
+          json.data.relationships.should.be.Object()
+          json.data.relationships.should.have.properties([
+            'campaigns',
+            'projects'
+          ])
+          json.included.should.be.Array()
+          json.included.should.have.length(1)
+          json.included[0].type.should.eql('project')
         })
         .end(done)
     })
