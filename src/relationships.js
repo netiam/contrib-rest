12f1c1146096ee1b85266fa3ad91ff6f78095987
Hannes Moser <hannes@impossiblearts.com>
2016-03-23 11:49:20 +0100

    chore: add constants for relationships
    The next release will contain relationship handling on resource level.
    Therefore a definition of relationships has been added for later us.

--- src/relationships.js
+++ src/relationships.js
@@ -1,9 +1,19 @@
 /**
  * @type {Symbol}
  */
+export const HAS_ONE = Symbol('has one')
+
+/**
+ * @type {Symbol}
+ */
 export const HAS_MANY = Symbol('has many')
 
 /**
  * @type {Symbol}
  */
 export const BELONGS_TO = Symbol('belongs to')
+
+/**
+ * @type {Symbol}
+ */
+export const BELONGS_TO_MANY = Symbol('belongs to many')
